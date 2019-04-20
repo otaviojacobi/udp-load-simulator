1c8c9463767dd42c6fd184d92d9eb750d6ac086b
Otavio Jacobi <o.jacobi@sap.com>
2019-04-20 19:11:46 -0300

    added first unit relevat unit test

--- test/unit-test/test_unit_cli.py
+++ test/unit-test/test_unit_cli.py
@@ -1,7 +1,11 @@
 import sys
 sys.path.append('./src')
 
+from unittest import mock
+import argparse
 from cli import CLIParser
 
-def test_answer():
-    assert 3 == 3
+@mock.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(bandwidth='1M'))
+def test_answer(mock_args):
+    cli_namespace = CLIParser().parse()
+    assert cli_namespace.bandwidth == 1000000
